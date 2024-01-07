a83a63f01c159cd483e4c3a9ec7a2cdf06fa3f73
KLEYER <38478356+RRINKIN@users.noreply.github.com>
2024-01-07 16:29:57 +0100

    styling drop down menu for desktop

--- Reseau_Bien_Etre/var/cache/dev/App_KernelDevDebugContainer.php
+++ Reseau_Bien_Etre/var/cache/dev/App_KernelDevDebugContainer.php
@@ -16,7 +16,7 @@ if (!\class_exists(App_KernelDevDebugContainer::class, false)) {
 
 return new \ContainerYYOAyNR\App_KernelDevDebugContainer([
     'container.build_hash' => 'YYOAyNR',
-    'container.build_id' => 'bd9ea466',
-    'container.build_time' => 1704548937,
+    'container.build_id' => '6856e5b8',
+    'container.build_time' => 1704575172,
     'container.runtime_mode' => \in_array(\PHP_SAPI, ['cli', 'phpdbg', 'embed'], true) ? 'web=0' : 'web=1',
 ], __DIR__.\DIRECTORY_SEPARATOR.'ContainerYYOAyNR');
