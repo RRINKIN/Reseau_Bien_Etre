952f0ca7f87ad398a088b727087603c2b908d587
KLEYER <38478356+RRINKIN@users.noreply.github.com>
2024-01-28 11:25:09 +0100

    making next & previous button dynamic

--- reseau_bien_etre_next/.next/static/chunks/webpack.js
+++ reseau_bien_etre_next/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "626bcc5c26dd9241"; }
+/******/ 		__webpack_require__.h = function() { return "fbf8af6cdb3bfc82"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- reseau_bien_etre_next/app/components/homePage/prestataireCards.js
+++ reseau_bien_etre_next/app/components/homePage/prestataireCards.js
@@ -4,7 +4,8 @@ import { useEffect, useState } from 'react';
 import { GetPrestataire } from '../../api/getPrestataire';
 import Cards from '../global/cardPrestataire';
 import CardsWithNew from '../global/cardPrestataire_with_new';
-import Pagination from "./pagination";
+import PaginationNext from "./paginationNext";
+import PaginationPrevious from "./paginationPrevious";
 
 function PrestataireCards() {
   const [prestataireCard, setPrestataireCard] = useState();
@@ -12,8 +13,24 @@ function PrestataireCards() {
   // collect the URL
   const [urlPrestataire, setUrlPrestataire] = useState('/api/prestataires');
   const [nextUrlPrestataire, setNextUrlPrestataire] = useState('');
+  const [previousUrlPrestataire, setPreviousUrlPrestataire] = useState('');
   const prestataireUrlToUseforNext = (e) => {
-    setUrlPrestataire(`${nextUrlPrestataire.toString()}`);
+    //setUrlPrestataire(`${nextUrlPrestataire.toString()}`);
+    if (nextUrlPrestataire !== undefined) {
+      setUrlPrestataire(`${nextUrlPrestataire.toString()}`);
+    } else {
+      // set url to an empty string
+      setUrlPrestataire('/api/prestataires');
+    }
+  }
+  const prestataireUrlToUseforPrevious = (a) => {
+    //setUrlPrestataire(`${previousUrlPrestataire.toString()}`);
+    if (previousUrlPrestataire !== undefined) {
+      setUrlPrestataire(`${previousUrlPrestataire.toString()}`);
+    } else {
+      // set url to an empty string
+      setUrlPrestataire('/api/prestataires');
+    }
   }
 
   // fonction d'appel de l'API services
@@ -26,7 +43,8 @@ function PrestataireCards() {
       res => {
         const prestataireCards = [];
         let nextApiUrl = res['hydra:view']['hydra:next'];
-        //let previousApiUrl = res['hydra:view']['hydra:previous'];
+        let previousApiUrl = res['hydra:view']['hydra:previous'];
+        console.log(previousApiUrl, 'previous');
         let counter = 0;
         for (const cardsData of res['hydra:member']) {
           if (counter < 4) {
@@ -38,7 +56,18 @@ function PrestataireCards() {
         }
         setPrestataireCard(prestataireCards);
         setNextUrlPrestataire(nextApiUrl);
+        setPreviousUrlPrestataire(previousApiUrl);
       })
+    }
+
+    let displayPreviousButton;
+    if (previousUrlPrestataire != undefined) {
+      displayPreviousButton = <PaginationPrevious onPaginationPreviousClick={prestataireUrlToUseforPrevious} />;
+    } 
+
+    let displayNextButton;
+    if (nextUrlPrestataire != undefined) {
+      displayNextButton = <PaginationNext onPaginationNextClick={prestataireUrlToUseforNext}/>;
     }
 
   useEffect(() => {
@@ -52,7 +81,10 @@ function PrestataireCards() {
       <ul className="flex flex-col md:flex-row items-center flex-wrap max-w-screen-lg">
         {prestataireCard}
       </ul>
-      <Pagination onPaginationNextClick={prestataireUrlToUseforNext} />
+      <div className='flex flex-row justify-around md:w-1/3'>
+        {displayPreviousButton}
+        {displayNextButton}
+      </div>
     </>
   )
 }
